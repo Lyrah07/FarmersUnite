cd9531534c8053fa8fe3209ca243d5c9edaf6764
Lyrah07 <buccatharyl@gmail.com>
2024-11-05 22:00:20 +0800

    Continued and created new files for the Back-End development of the app's features

--- app/src/main/java/com/example/farmersunite/Activity/ShopActivity.java
+++ app/src/main/java/com/example/farmersunite/Activity/ShopActivity.java
@@ -1,26 +1,82 @@
 package com.example.farmersunite.Activity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.farmersunite.Adapter.CategoryAdapter;
+import com.example.farmersunite.Adapter.TopProductsAdapter;
+import com.example.farmersunite.Domain.CategoryDomain;
+import com.example.farmersunite.Domain.ItemsDomain;
 import com.example.farmersunite.R;
 
+import java.util.ArrayList;
+
 public class ShopActivity extends AppCompatActivity {
+    private RecyclerView.Adapter catAdapter,topProductsAdapter;
+    private RecyclerView recyclerViewCat,recyclerViewTopProducts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_shop);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        initRecyclerviewCat();
+        initLocation();
+        initRecyclerViewTopProducts();
+    }
+
+    private void initLocation() {
+        String[] items=new String[]{"Baguio City, Philippines","Manila, Philippines","Cebu, Philippines"};
+        final Spinner locationSpin= findViewById(R.id.spinner);
+        ArrayAdapter<String> adapter=new ArrayAdapter<>(this, android.R.layout.simple_spinner_item,items);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        locationSpin.setAdapter(adapter);
+    }
+
+    private void initRecyclerviewCat() {
+        ArrayList<CategoryDomain> items=new ArrayList<>();
+        items.add(new CategoryDomain("cat1","Vegetable"));
+        items.add(new CategoryDomain("cat2","Fruit"));
+        items.add(new CategoryDomain("cat3","Dairy"));
+        items.add(new CategoryDomain("cat4","Drinks"));
+        items.add(new CategoryDomain("cat5","Grain"));
+
+        recyclerViewCat=findViewById(R.id.catView); //catView??//
+        recyclerViewCat.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false));
+
+        catAdapter = new CategoryAdapter(items);
+        recyclerViewCat.setAdapter(catAdapter);
+
+    }
+    private void initRecyclerViewTopProducts(){
+        recyclerViewTopProducts=findViewById(R.id.bestView);
+        recyclerViewTopProducts.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false));
+
+        topProductsAdapter = new TopProductsAdapter(getData());
+        recyclerViewTopProducts.setAdapter(topProductsAdapter);
+    }
+
+    public ArrayList<ItemsDomain> getData(){
+        ArrayList<ItemsDomain>items = new ArrayList<>();
+        items.add(new ItemsDomain("orange","Fresh Orange",111.2,"Asim Kilig - Lorem ipsum Occaecat do esse ex et dolor culpa" +
+                "nisi ex in magna consectetur nisi cupidatat laboris esse" +
+                "eiusmod deserunt aute do quis velit esse sed Ut proident" +
+                "cupidatat nulla esse cillum laborum occaecat nostrud sit" +
+                "dolor incididunt amet est occaecat nisi.",4.2));
+        items.add(new ItemsDomain("apple","Fresh apple",50.5,"Crispy Licious",4.3));
+        items.add(new ItemsDomain("watermelon","Fresh watermelon",143,"Juicy Licious",4.8));
+        items.add(new ItemsDomain("berry","Fresh berry",220.3,"Smallicious",4));
+        items.add(new ItemsDomain("pineaplle","Fresh pineapple",86.8,"Pinellicious",3.7));
+        items.add(new ItemsDomain("strawberry","Fresh strawberry",300,"Licious..",4.5));
+        return items;
     }
 }
---
+++ app/src/main/java/com/example/farmersunite/Adapter/TopProductsAdapter.java
@@ -0,0 +1,72 @@
+package com.example.farmersunite.Adapter;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.farmersunite.Activity.DetailActivity;
+import com.example.farmersunite.Domain.ItemsDomain;
+import com.example.farmersunite.R;
+
+import java.util.ArrayList;
+
+public class TopProductsAdapter extends RecyclerView.Adapter<TopProductsAdapter.viewholder> {
+    ArrayList<ItemsDomain> items;
+    Context context;
+
+    public TopProductsAdapter(ArrayList<ItemsDomain> items) {
+        this.items = items;
+    }
+
+    @NonNull
+    @Override
+    public TopProductsAdapter.viewholder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        context = parent.getContext();
+        View inflate = LayoutInflater.from(parent.getContext()).inflate(R.layout.viewholder_top_products,parent,false);
+        return new viewholder(inflate);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull TopProductsAdapter.viewholder holder, int position) {
+        holder.titleTxt.setText(items.get(position).getTitle());
+        holder.priceTxt.setText(items.get(position).getPrice()+"₱/kg");
+
+        int drawableResourceId = holder.itemView.getResources()
+                .getIdentifier(items.get(position).getImgPath(),"drawable",holder.itemView.getContext().getPackageName());
+
+        Glide.with(context)
+                .load(drawableResourceId)
+                .into(holder.pic);
+
+        holder.itemView.setOnClickListener(v -> {
+            Intent intent = new Intent(context, DetailActivity.class);
+            intent.putExtra("object", items.get(position));
+            context.startActivity(intent);
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+
+    public class viewholder extends RecyclerView.ViewHolder{
+        TextView titleTxt,priceTxt;
+        ImageView pic;
+        public viewholder(@NonNull View itemView) {
+            super(itemView);
+            titleTxt = itemView.findViewById(R.id.titleTxt);
+            priceTxt = itemView.findViewById(R.id.priceTxt);
+            pic = itemView.findViewById(R.id.img);
+        }
+    }
+}
